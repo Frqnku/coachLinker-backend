fb6dd16457fc5f9f0397db885cbaae10a32c1f4c
yacine <yacine.lariane@gmail.com>
2023-08-01 13:33:01 +0200

    postCloudinary

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const uniqid = require('uniqid');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+const uniqId = uniqid();
+router.post('/upload', async (req, res) => {
+    
+    
+    const photoPath = `./tmp/${uniqId}.jpg`;
+    const resultMove = await req.files.photoFromFront.mv(photoPath);
+   const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    
+    if(!resultMove) {
+    fs.unlinkSync(photoPath);
+        res.json({ result: true, url: resultCloudinary.secure_url });     
+    } else {
+      res.json({ result: false, error: resultMove });
+    }
+   });
 
 module.exports = router;
