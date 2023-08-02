ac6b109ac8b97afdf609b52b4d8f06890ea1f000
yacine <yacine.lariane@gmail.com>
2023-08-02 15:30:11 +0200

    backUpdatePicture

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,28 @@ const uniqid = require('uniqid');
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 
+
 router.post('/upload', async (req, res) => {
-    console.log('fetched')
-    const uniqId = uniqid();
-    
-    const photoPath = `./tmp/${uniqId}.jpg`;
-    const resultMove = await req.files.photoFromFront.mv(photoPath);
-    
-    if(!resultMove) {
-   const resultCloudinary = await cloudinary.uploader.upload(photoPath);
-    fs.unlinkSync(photoPath);
-        res.json({ result: true, url: resultCloudinary.secure_url });     
-    } else {
-      res.json({ result: false, error: resultMove });
-    }
-   });
+  try {
+    const photoStream = req.files.photoFromFront.data;
+
+    cloudinary.uploader.upload_stream(
+      { resource_type: 'image' },
+      (error, result) => {
+        if (error) {
+          console.error('Error uploading to Cloudinary:', error);
+          res.json({ result: false, error: 'Error uploading to Cloudinary' });
+        } else {
+          console.log('Cloudinary response:', result);
+          res.json({ result: true, url: result.secure_url });
+        }
+      }
+    ).end(photoStream);
+  } catch (error) {
+    console.error('Error handling the upload:', error);
+    res.json({ result: false, error: 'Error handling the upload' });
+  }
+});
+
 
 module.exports = router;
