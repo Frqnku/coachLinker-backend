b72116f0e8f1eb699f5e11b374e17375f67e715f
Maud <ali-5@live.fr>
2023-08-04 10:39:20 +0200

    save before pull

--- models/students.js
+++ models/students.js
@@ -10,7 +10,7 @@ const studentSchema = mongoose.Schema({
   image: String,
   dateOfBirth : String,
   myDescription : String,
-  favoriteSport : [String],
+  favoriteSport :Array,
 });
 
 const Student = mongoose.model('students', studentSchema);
